36a18c8d782272da6260acaa3442f7cd73f8b83c
Kateryna Gordiienko <gordkat@gmail.com>
2023-01-30 16:24:40 +0100

    address

--- dreamertattoo/src/views/Contact.jsx
+++ dreamertattoo/src/views/Contact.jsx
@@ -1,7 +1,21 @@
 import styles from "./Contact.module.css";
 
 const Contact = () => {
-  return <main>Contact</main>;
+  return (
+    <main className={styles.containerContact}>
+      <div className={styles.wrapperContact}>
+        <h2 className={styles.titleContact}>dreamer tattoo</h2>
+        <p className={styles.address}>
+          Via colombare, 3
+          <br />
+          vicino Porto Galeazzi
+          <br />
+          Sirmione, Italy
+        </p>
+        <div>Mappa</div>
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
